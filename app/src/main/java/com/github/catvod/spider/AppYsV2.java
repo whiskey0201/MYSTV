6c3d5d556accdf5c483225f588e97c180191b6c9
彭于晏 <85066340+ks5200@users.noreply.github.com>
2022-03-18 08:24:50 +0800

    Update AppYsV2.java

--- app/src/main/java/com/github/catvod/spider/AppYsV2.java
+++ app/src/main/java/com/github/catvod/spider/AppYsV2.java
@@ -101,8 +101,6 @@ public class AppYsV2 extends Spider {
                                     typeN = "语言";
                                 else if (type.equals("year"))
                                     typeN = "年份";
-                                else if (type.equals("letter"))
-                                    typeN = "字母";
                             }
                             JSONObject jOne = new JSONObject();
                             jOne.put("key", type);
@@ -212,7 +210,6 @@ public class AppYsV2 extends Spider {
             url = url.replace("筛选area", (extend != null && extend.containsKey("area")) ? extend.get("area") : "");
             url = url.replace("筛选lang", (extend != null && extend.containsKey("lang")) ? extend.get("lang") : "");
             url = url.replace("筛选year", (extend != null && extend.containsKey("year")) ? extend.get("year") : "");
-            url = url.replace("筛选letter", (extend != null && extend.containsKey("letter")) ? extend.get("letter") : "");
             url = url.replace("排序", (extend != null && extend.containsKey("排序")) ? extend.get("排序") : "");
             SpiderDebug.log(url);
             String json = desc(OkHttpUtil.string(url, getHeaders(url)), (byte) 2);
